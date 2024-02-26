6279575399e566aa3be064bd40f9cbe22a6b0720
mnlknt <63652959+mnlknt@users.noreply.github.com>
2024-02-26 19:41:43 +0100

    Update and rename PCA_spectral-clustering_ensamble_avg.py to Codes/PCA_spectral-clustering_ensamble_avg.py

--- Codes/PCA_spectral-clustering_ensamble_avg.py
+++ Codes/PCA_spectral-clustering_ensamble_avg.py
@@ -7,21 +7,14 @@ import numpy.linalg
 import matplotlib
 matplotlib.style.use("seaborn-talk")
 from sklearn import metrics
-#from sklearn.cluster import SpectralClustering
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.metrics import pairwise_distances
 
-
-#savedata = '/media/lorenzo/Backup4TBU3/User_feature_analysis/User_features_analysis_new/Data/With_user_filter_II/PCA_spectral_clustering/Same_sample_size/'
-path = '/mnt/TANK4TB/User_feature_analysis/User_features_analysis_new/Data/With_user_filter_II/User_standardized_features_new/'
-#savepath = '/media/lorenzo/Backup4TBU3/User_feature_analysis/User_features_analysis_new/Plot/With_user_filter_II/PCA/PCA_spectral_clustering/Same_sample_size/Graphs/No_noise/'
-savedata = '/mnt/TANK4TB/User_feature_analysis/User_features_analysis_new/Data/With_user_filter_II/PCA_ensamble_avg_new/Spectral_clustering/'
-
-#range_n_clusters = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+path = ''
+savedata = ''
 
 dropcols = ['spy','amd','tsla','mu','aapl','amzn','msft','snap','nvda','spce','fb','dis','bynd','nflx','jnug','ge','rad','sq','atvi','uso','twtr','amc','bb','nok','pltr','gme']
-
 
 w = [23, 54, 84, 115, 145, 176]
 
@@ -32,16 +25,11 @@ df = pd.DataFrame(0, index=np.arange(len(w)), columns=cols)
 nf = pd.DataFrame(0, index=np.arange(len(w)), columns=cols)
 cf = pd.DataFrame(0, index=np.arange(len(w)), columns=cols)
 
-#readpath = '/media/lorenzo/Backup4TBU3/User_feature_analysis/User_features_analysis_new/Data/With_user_filter_II/PCA_spectral_clustering/Same_sample_size/'
-
 for j in range(0,100):
     
     clusters = []
     clus_numb = []
     noise = []
-       
-    #nodes = []
-    #edges = []
      
     for i in w:
         
@@ -50,9 +38,7 @@ for j in range(0,100):
     
         features = features.sample(10_000) #prende ogni volta delle sample diverse
         features.reset_index(drop = True, inplace = True)
-        
-        #features.to_csv(savedata + 'features_sample_{}.csv'.format(i))
-        
+                
         M = features.drop(columns = ['author'])
         
         pca = PCA(n_components = 10)
@@ -74,18 +60,11 @@ for j in range(0,100):
         
         #add to noise ponts from connected components with less than 5 nodes
         cc = [c for c in sorted(nx.connected_components(g), key=len, reverse=True)]
-        
-        # labels = []
-        
+                
         for comp in cc:
             if len(comp) < 5:
                 isolates.extend(list(comp))
-        #     else:
-        #         labels.append(list(comp))
-                
-        # labs = pd.Series(labels)
-        # labs.to_csv(savedata + 'connected_comp_labels_week_{}.csv'.format(i))
-        
+    
         isolates = list(set(isolates))
         
         g.remove_nodes_from(isolates)
@@ -93,18 +72,6 @@ for j in range(0,100):
         noise.append(len(isolates))
         
         f = [k for k in l if k >= 5]
-        
-        # fig = plt.figure(figsize=(10, 10))
-        # nx.draw_networkx(g, node_size = 10, alpha = 0.8, width = 1, node_color = 'red', with_labels = False)
-        # plt.tight_layout()
-        # plt.show()
-        # fig.savefig(savepath + "graph_week_{}.png".format(i))   
-        
-        #n_nodes = g.number_of_nodes()
-        #n_edges = g.number_of_edges()
-        
-        #nodes.append(n_nodes)
-        #edges.append(n_edges)
         
         clusters.append(f)
         clus_numb.append(len(f))
