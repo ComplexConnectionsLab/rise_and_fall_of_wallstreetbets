1fcbd556dc8baa0143ef066e1b2d2e4ccccaf228
mnlknt <63652959+mnlknt@users.noreply.github.com>
2024-02-26 19:39:31 +0100

    Update and rename PCA_DBSCAN_ensamble_avg.py to Codes/PCA_DBSCAN_ensamble_avg.py

--- Codes/PCA_DBSCAN_ensamble_avg.py
+++ Codes/PCA_DBSCAN_ensamble_avg.py
@@ -3,24 +3,12 @@ import pandas as pd
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.decomposition import PCA
-#import matplotlib.pyplot as plt
-#import matplotlib
-#matplotlib.style.use("seaborn-dark")
 from tqdm import tqdm
-#from sklearn.manifold import TSNE
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# import matplotlib
-# matplotlib.style.use("seaborn-dark")
 
-path = '/mnt/TANK4TB/User_feature_analysis/User_features_analysis_new/Data/With_user_filter_II/User_standardized_features_new/'
-savedata = '/mnt/TANK4TB/User_feature_analysis/User_features_analysis_new/Data/With_user_filter_II/PCA_ensamble_avg_new/DBSCAN/All_weeks/'
+path = ''
+savedata = ''
 
 dropcols = ['spy','amd','tsla','mu','aapl','amzn','msft','snap','nvda','spce','fb','dis','bynd','nflx','jnug','ge','rad','sq','atvi','uso','twtr','amc','bb','nok','pltr','gme']
-
-#w = [0, 20, 51, 81, 112, 142, 173]
-#w = [23, 54, 84, 115, 145, 176]
-#w = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105, 112, 119, 126, 133, 140, 147, 154, 161, 168, 175]
 
 cols = list(np.arange(0,100))
 cols = list(map(str, cols))
@@ -28,11 +16,9 @@ cols = list(map(str, cols))
 df = pd.DataFrame(0, index=np.arange(178), columns=cols)
 nf = pd.DataFrame(0, index=np.arange(178), columns=cols)
 cf = pd.DataFrame(0, index=np.arange(178), columns=cols)
-#sf = pd.DataFrame(0, index=np.arange(len(w)), columns=cols)
 
 for j in tqdm(range(0, 100)):
 
-    #silscore = []
     noisepoints = []
     clusters = []
     sizes = []
@@ -57,7 +43,6 @@ for j in tqdm(range(0, 100)):
          
         noisepoints.append(n_noise_)
         clusters.append(l)
-        #silscore.append(metrics.silhouette_score(X, db.labels_))
         
         count = pd.Series(db.labels_).value_counts()
         count = count[count.index != -1]
@@ -67,10 +52,8 @@ for j in tqdm(range(0, 100)):
     df['{}'.format(j)] = clusters
     nf['{}'.format(j)] = noisepoints
     cf['{}'.format(j)] = sizes
-    #sf['{}'.format(j)] = silscore
 
 
 df.to_csv(savedata + 'cluster_numbers.csv')
 nf.to_csv(savedata + 'noisepoints.csv')
 cf.to_csv(savedata + 'cluster_sizes.csv')
-#sf.to_csv(savedata + 'silhouette.csv')
