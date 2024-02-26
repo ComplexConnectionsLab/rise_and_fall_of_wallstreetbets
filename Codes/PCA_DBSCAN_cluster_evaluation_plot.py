1bd67737d73a87186f15bb2fc0ac1a8ca75ae96f
mnlknt <63652959+mnlknt@users.noreply.github.com>
2024-02-26 19:38:30 +0100

    Update and rename PCA_DBSCAN_cluster_evaluation_plot.py to Codes/PCA_DBSCAN_cluster_evaluation_plot.py

--- Codes/PCA_DBSCAN_cluster_evaluation_plot.py
+++ Codes/PCA_DBSCAN_cluster_evaluation_plot.py
@@ -5,8 +5,8 @@ import matplotlib
 matplotlib.style.use("seaborn-talk")
 import matplotlib.ticker as mticker
 
-path = '/mnt/TANK4TB/User_feature_analysis/User_features_analysis_new/Data/With_user_filter_II/PCA_cluster_evaluation/D-B_index/'
-savepath = '/mnt/TANK4TB/User_feature_analysis/User_features_analysis_new/Plot/Paper_figures/'
+path = ''
+savepath = ''
 
 week = np.arange(0,178)
 settimane = ['01/08-07/08', '20/09-26/09', '9/11-15/11', '29/12-04/01']
@@ -65,42 +65,11 @@ for i in range(0,3):
         if (j == 0):
             ax[i][j].set_ylabel('Fraction of noise', fontsize = 20)
             
-        # ax[i][j].set_xlabel('Week', fontsize = 15)
-        # ax[i][j].set_ylabel('Fraction of noise', fontsize = 15)
-        # ax[i][j].tick_params(axis='x', labelsize = 10, rotation = 45)
-        # ax[i][j].tick_params(axis='y', labelsize = 10)
-        # #ax[i][j].set_yscale('log')
-        
-        # #ax2.set_xlabel('Week', fontsize = 30)
-        # ax2.set_ylabel('D-B index', fontsize = 15, rotation = 270)
-        # ax2.tick_params(axis='x', labelsize = 15, rotation = 45)
-        # ax2.tick_params(axis='y', labelsize = 10)
-        # #ax2.legend(fontsize = 10, frameon = False, loc = (1.1, 0.57))
-        # #ax2.yaxis.set_label_coords(1.08, .5)
-        
-        # for axis in ['top','bottom','left','right']:
-        #     ax[i][j].spines[axis].set_linewidth(2)
-        
-        # # increase tick width
-        # ax[i][j].tick_params(width=2)
-        
-        # # ticks_loc = ax.get_xticks().tolist()
-        # # ticks_loc = ticks_loc[1:9]
-        # # ax.xaxis.set_major_locator(mticker.FixedLocator(ticks_loc))
-        # # ax.set_xticklabels(settimane)
-        
-        # ax[i][j].spines['right'].set_visible(True)
-        # ax[i][j].spines['top'].set_visible(True)
-        #ax[i][j].legend(fontsize = 10, frameon = False, loc = (1.1, 0.67))
         ax2.legend(loc = 'upper right', frameon = False)
         ax[i][j].legend(loc = 'upper left', frameon = False)
         
         ax[i][j].set_title('eps = {}, min_samples = {}'.format(epsval[i], minsampval[j]))
         l += 1
-#ax[0].set_title('Spectral clustering', fontsize = 20)ù
-
-
-#plt.legend(fontsize = 20)
 plt.tight_layout()
 plt.savefig(savepath + 'DBSCAN_cluster_evaluation.pdf', dpi = 300)
 
