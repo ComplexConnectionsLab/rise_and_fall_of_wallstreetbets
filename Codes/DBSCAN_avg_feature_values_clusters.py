aa7ea32e4c104eae79d259704a65d9e42d6c0c2d
mnlknt <63652959+mnlknt@users.noreply.github.com>
2024-02-26 19:36:01 +0100

    Update and rename DBSCAN_avg_feature_values_clusters.py to Codes/DBSCAN_avg_feature_values_clusters.py

--- Codes/DBSCAN_avg_feature_values_clusters.py
+++ Codes/DBSCAN_avg_feature_values_clusters.py
@@ -5,9 +5,8 @@ import matplotlib
 matplotlib.style.use("seaborn-talk")
 import seaborn as sns
 
-#dbsdata = '/mnt/TANK4TB/User_feature_analysis/User_features_analysis_new/Data/With_user_filter_II/PCA_same_samples_final/DBSCAN/eps_1/'
-dbsdata = '/mnt/TANK4TB/User_feature_analysis/User_features_analysis_new/Data/With_user_filter_II/PCA_same_samples_final/DBSCAN/eps_1/min_samples_15/'
-saveplot = '/mnt/TANK4TB/User_feature_analysis/User_features_analysis_new/Plot/Paper_figures/'
+dbsdata = ''
+saveplot = ''
 
 weeks = ['24-30 Aug', '24-30 Sept', '24-30 Oct', '24-30 Nov', '24-30 Dec', '24-30 Jan']
 
@@ -62,11 +61,8 @@ std3 = np.asarray(std3)
 fig, ax = plt.subplots(figsize = (10,10))
 
 ax.plot(feats, both1_m, linewidth = 4, alpha = 1, c = '#04E762', label = "Cluster 1: 5058")
-#ax.fill_between(feats, both1_m-std1, both1_m+std1, color = '#04E762', alpha = 0.3)
 ax.plot(feats, both2_m, linewidth = 4, alpha = 1, c = '#DC0073', label="Cluster 2: 3607")
-#ax.fill_between(feats, both2_m-std2, both2_m+std2, color = '#DC0073', alpha = 0.3)
 ax.plot(feats, both3_m, linewidth = 4, alpha = 1, c = '#F5B700', label="Cluster 3: 364")
-#ax.fill_between(feats, both3_m-std3, both3_m+std3, color = '#F5B700', alpha = 0.3)
 
 ax.set_xlabel('Features', fontsize = 30)
 ax.set_ylabel('Avereage value', fontsize = 30)
@@ -89,9 +85,6 @@ fig.savefig(saveplot + "top_3_clusters_features_plot_2.svg")
 
 
 
-
-
-
 #posters f7b500, active de0074, commenters 04e756
 colors = ['#04e756', '#de0074', '#f7b500']
 
@@ -110,9 +103,6 @@ ax.yaxis.grid(True, alpha = 0.5, linewidth = 0.8)
 p = sns.violinplot(data=tot, x="Features", y="value", hue="labels", cut = 0, linewidth= 0.5, palette = colors, saturation = 1, 
                scale = 'width', inner = 'box')
 
-# sns.violinplot(data=tot, x="Features", y="value", hue="labels", order = ['num_comm','comm_score','first_children','comm_entropy','num_post','post_score','post_comms','post_entropy','k_in','k_out'],
-#                hue_order = ['0', '1', '2'], dodge = True, palette = colors)
-
 for axis in ['top','bottom','left','right']:
     ax.spines[axis].set_linewidth(2)
 
@@ -123,10 +113,7 @@ ax.set_xlabel('Features', fontsize = 20)
 ax.set_ylabel('Standardized values', fontsize = 20)
 ax.tick_params(axis='x', labelsize = 15, rotation = 45)
 ax.tick_params(axis='y', labelsize = 15)
-# Improve the legend
 p.legend(handles=p.legend_.legendHandles, labels=['Commenters', 'Active', 'Posters'], fontsize = 15, frameon = False, ncol = 3, loc = (0.04, 0.94))
-#ax.legend(p, loc = (0.04, 0.94), ncol = 3, fontsize = 15, frameon = False, title = None, columnspacing = 1, handletextpad = 0)
-#sns.move_legend(ax, loc = 'upper left', ncol = 3, fontsize = 15, frameon = False, title = None, columnspacing = 1, handletextpad = 0)
 plt.tight_layout()
 f.savefig(saveplot + 'avg_features_clusters_violin_boxplot.svg')
 
@@ -137,16 +124,6 @@ matplotlib.rcParams['lines.solid_capstyle'] = 'round'
 f, ax = plt.subplots(figsize = (22,8))
 ax.axhline(0, color = 'black', linewidth = '1', ls= '--')
 
-#sns.despine(bottom = True, left = True)
-
-# Show each observation with a scatterplot
-# sns.stripplot(
-#     data = totals, x = "Values", y = "Feature", hue = "Group",
-#     dodge = True, alpha = .25, zorder = 1, legend=False)
-
-# Show the conditional means, aligning each pointplot in the
-# center of the strips by adjusting the width allotted to each
-# category (.8 by default) by the number of hue levels
 p = sns.pointplot(
     #data = tot, x = "value", y = "Features", hue = "labels", 
     data = tot, x = "Features", y = "value", hue = "labels",
@@ -179,60 +156,6 @@ p.legend(handles=p.legend_.legendHandles, labels=['Commenters', 'Active', 'Poste
 #sns.move_legend(ax, loc = 'upper left', ncol = 1, fontsize = 15, frameon = False, title = None, columnspacing = 1, handletextpad = 0)
 plt.tight_layout()
 f.savefig(saveplot + 'avg_features_clusters_pointplot_new.pdf', dpi = 300)
-
-
-
-
-
-
-
-
-
-# f, ax = plt.subplots(figsize = (10,10))
-# ax.axvline(0, color = 'red', linewidth = '2')
-
-# #sns.despine(bottom = True, left = True)
-
-# # Show each observation with a scatterplot
-# # sns.stripplot(
-# #     data = totals, x = "Values", y = "Feature", hue = "Group",
-# #     dodge = True, alpha = .25, zorder = 1, legend=False)
-
-# # Show the conditional means, aligning each pointplot in the
-# # center of the strips by adjusting the width allotted to each
-# # category (.8 by default) by the number of hue levels
-# sns.stripplot(
-#     data = tot, x = "value", y = "Features", hue = "labels",
-#     dodge=True, alpha=.2, legend=False, palette = colors)
-
-# sns.pointplot(
-#     data = tot, x = "value", y = "Features", hue = "labels",
-#     order = ['num_comm','comm_score','first_children','comm_entropy','num_post','post_score','post_comms','post_entropy','k_in','k_out'],
-#     hue_order = [0, 1, 2],
-#     join = False, dodge = .8 - .8 / 6, palette = colors,
-#     markers = "_", scale = 0.8, errorbar=None)
-    
-
-# ax.set_xscale('symlog')
-# for axis in ['top','bottom','left','right']:
-#     ax.spines[axis].set_linewidth(2)
-
-# # increase tick width
-# ax.tick_params(width=2)
-
-# #ax.set_xlim(-3, 2)
-
-# ax.set_xlabel('Standardized values', fontsize = 20)
-# ax.set_ylabel('Features', fontsize = 20)
-# ax.tick_params(axis='x', labelsize = 20)
-# ax.tick_params(axis='y', labelsize = 20)
-# ax.xaxis.grid(True, alpha = 0.5, linewidth = 0.8)
-# # Improve the legend
-# p.legend(handles=p.legend_.legendHandles, labels=['Commenters', 'Active', 'Posters'], fontsize = 20, frameon = False, ncol = 3, loc = (0, 1.02))
-# #sns.move_legend(ax, loc = 'upper left', ncol = 1, fontsize = 15, frameon = False, title = None, columnspacing = 1, handletextpad = 0)
-# plt.tight_layout()
-# f.savefig(saveplot + 'avg_features_clusters_pointplot.pdf', dpi = 300)
-
 
 
 
